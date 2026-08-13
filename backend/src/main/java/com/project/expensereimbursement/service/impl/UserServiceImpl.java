0f3c27ec435eea2f564107e9861970c4ea7e3c07
dobhalsaurish <saurishdobhal@gmail.com>
2026-08-13 14:42:43 +0530

    Update: Added buisness logic in UserService

--- backend/src/main/java/com/project/expensereimbursement/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/project/expensereimbursement/service/impl/UserServiceImpl.java
@@ -2,11 +2,15 @@ package com.project.expensereimbursement.service.impl;
 
 import com.project.expensereimbursement.dto.request.CreateUserRequest;
 import com.project.expensereimbursement.dto.response.UserResponse;
+import com.project.expensereimbursement.entity.User;
+import com.project.expensereimbursement.enums.Role;
+import com.project.expensereimbursement.exception.EmailAlreadyExistsException;
 import com.project.expensereimbursement.repository.UserRepository;
+import com.project.expensereimbursement.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
@@ -14,9 +18,28 @@ public class UserServiceImpl {
         this.userRepository = userRepository;
     }
 
+    @Override
     public UserResponse createUser(CreateUserRequest request){
 
-        // Business logic
-        return null;
+        if(userRepository.existByEmail(request.getEmail())){
+            throw new EmailAlreadyExistsException(
+                    "Email already exists"
+            );
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .role(Role.EMPLOYEE)
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        return UserResponse.builder()
+                .userId(savedUser.getUserId())
+                .name(savedUser.getName())
+                .email(savedUser.getEmail())
+                .role(savedUser.getRole())
+                .build();
     }
 }
